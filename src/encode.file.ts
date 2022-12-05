744a21f6fbca06be152374a5d3263849832cfc57
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-12-06 00:15:52 +0100

    add `getInfo` function

--- src/encode.file.ts
+++ src/encode.file.ts
@@ -1,7 +1,31 @@
+import { parseDuration, parseProgress } from './parse.ffmpeg.output';
+
+import { parentPort } from 'worker_threads';
 import { spawn } from 'child_process';
 
+const getInfo = async (inputFile: string) => {
+  return new Promise<any>((resolve, reject) => {
+    const chunks: string[] = [];
+    const infoProc = spawn('ffmpeg', ['-i', inputFile]);
+
+    infoProc.stderr.on('data', chunk => {
+      chunks.push(chunk);
+    });
+
+    infoProc.on('close', () => {
+      resolve(chunks.join('\n'));
+    });
+    // infoProc.on('error', err => reject(err));
+  });
+};
+
 export const encodeFile = async (inputFile: string, outputFile: string) => {
-  return new Promise<number | null>((resolve, reject) => {
+  return new Promise<number | null>(async (resolve, reject) => {
+    const info = await getInfo(inputFile);
+    const duration = parseDuration(info);
+
+    parentPort?.postMessage({ duration });
+
     const child = spawn('ffmpeg', [
       '-i',
       inputFile,
@@ -22,16 +46,16 @@ export const encodeFile = async (inputFile: string, outputFile: string) => {
     //   console.log('got data', chunk?.length);
     // });
 
-    // child.stderr.pipe(process.stderr);
-
-    // let errEvents = 0;
-    // child.stderr.on('data', chunk => {
-    //   if (errEvents > 6) {
-    //     const buffer = Buffer.from(chunk);
-    //     process.stdout.write(buffer);
-    //   }
-    //   errEvents++;
-    // });
+    child.stderr.on('data', chunk => {
+      const str = Buffer.from(chunk).toString();
+      if (str.includes('frame=')) {
+        const startIdx = str.lastIndexOf('frame=');
+        const endIdx = str.lastIndexOf('speed=') + 15;
+        const outputStr = str.substring(startIdx - 6, endIdx).trim();
+        const parsed = parseProgress(outputStr);
+        parentPort?.postMessage(parsed);
+      }
+    });
 
     child.on('error', err => {
       reject(err);
