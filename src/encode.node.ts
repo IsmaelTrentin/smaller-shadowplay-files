d3dfcd9f870cfc1a3af50d1274bb4266e6004d2e
Ismael Trentin <ismael.trentin@samtrevano.ch>
2022-12-06 00:16:06 +0100

    add progress bars

--- src/encode.node.ts
+++ src/encode.node.ts
@@ -1,8 +1,13 @@
+import ProgressBar from 'progress';
 import { Worker } from 'worker_threads';
 import fs from 'fs/promises';
 import { getItems } from './get.items';
+import { logger } from './logger';
+import { parseTimestamp } from './parse.ffmpeg.output';
 import path from 'path';
 import { queue } from '.';
+
+const barsMap = new Map<number, { latestTime: number; bar: ProgressBar }>();
 
 export const encodeNode = async (nodePath: string, outDirRoot: string) => {
   const [files, dirs] = await getItems(nodePath);
@@ -14,7 +19,9 @@ export const encodeNode = async (nodePath: string, outDirRoot: string) => {
     await fs.mkdir(outDir, { recursive: true });
   } catch (error) {
     if (error.code === 'EEXIST') {
-      console.log('out dir already exists, ignorig error');
+      logger.error('out dir already exists, ignorig error', {
+        at: 'encodeNode.mkdir',
+      });
     } else {
       throw error;
     }
@@ -37,8 +44,13 @@ export const encodeNode = async (nodePath: string, outDirRoot: string) => {
     return new Promise<number | null>((resolve, reject) => {
       const worker = new Worker(path.join(__dirname, 'worker.js'));
       const id = worker.threadId;
-      console.log(`Running worker ${id} for node ${nodePath}`);
-      console.log(`[${id}][NODE] ${dirs.length} dirs | ${files.length} files`);
+
+      logger.info(`Running worker ${id} for node ${nodePath}`, {
+        at: 'encodeNode.runWorker',
+      });
+      logger.info(`${dirs.length} dirs | ${files.length} files`, {
+        at: `worker.${id}`,
+      });
 
       const payload = {
         id,
@@ -48,7 +60,39 @@ export const encodeNode = async (nodePath: string, outDirRoot: string) => {
       worker.postMessage(payload);
 
       worker.on('message', msg => {
-        console.log(`[${id}] ${msg}`);
+        // exit code
+        if (typeof msg === 'number') {
+          if (msg === 0) {
+            barsMap.delete(id);
+          }
+          // current file duration
+        } else if (msg?.duration != undefined) {
+          const entryData = {
+            latestTime: 0,
+            bar: new ProgressBar(`[${id}] encoding [:bar] :percent :etas`, {
+              complete: '=',
+              incomplete: ' ',
+              width: 36,
+              total:
+                msg.duration.s + msg.duration.m * 60 + msg.duration.h * 120,
+            }),
+          };
+          barsMap.set(id, entryData);
+          // progress data (frames, framerate, q, time, ...)
+        } else if (typeof msg === 'object') {
+          const entry = barsMap.get(id);
+          if (!entry) return;
+          const timeObj = parseTimestamp(msg.time);
+          const sum = timeObj.s + timeObj.m * 60 + timeObj.h * 120;
+          const tick = sum - entry.latestTime;
+          entry.latestTime = sum;
+          entry.bar.tick(tick);
+          console.log();
+        } else {
+          logger.info(msg, {
+            at: `worker.${id}.message`,
+          });
+        }
       });
 
       worker.on('error', err => reject(err));
